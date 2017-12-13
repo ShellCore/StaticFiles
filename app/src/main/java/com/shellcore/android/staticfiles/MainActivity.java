693c444999bb9cd705ee3b76e9e8e2aae28ba979
Cesar Morales Garduño <shell.one.core@gmail.com>
2017-12-12 22:38:54 -0600

    Implementación de la lectura de un archivo que se encuentra en el directorio de recursos "raw"

--- app/src/main/java/com/shellcore/android/staticfiles/MainActivity.java
+++ app/src/main/java/com/shellcore/android/staticfiles/MainActivity.java
@@ -1,13 +1,49 @@
 package com.shellcore.android.staticfiles;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Components
+    @BindView(R.id.txt_content)
+    TextView txtContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        String data = readFile(this, R.raw.sandwich);
+        txtContent.setText(data);
+    }
+
+    public static String readFile(Context context, int resId) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(context.getResources().openRawResource(resId)));
+
+        String currentLine;
+
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            while ((currentLine = bufferedReader.readLine()) != null) {
+                sb.append(currentLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return sb.toString();
     }
 }
